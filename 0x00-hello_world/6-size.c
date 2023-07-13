ac47b4b6c6176b4f4f260159a902dfc042f450f9
KyakuwaPeninnah <kyakuwapeninnah@gmail.com>
2023-07-13 11:51:44 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,17 +4,11 @@
  * Return: 0 (success)
  */
 int main(void)
-{
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-
-printf("Size of a char: %lu byte(s)\n", (unsined long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (unsined long)sizeof(b));
-printf("Size of a long int: %lu bytes(s)\n", (unsined long)sizeof(c));
-printf("Size of a long long int: %lu bytes(s)\ni", (unsined long)sizeof(d));
-printf("Size of a float: %lu bytes(s)\n", (unsined long)sizeof(f));
+	}
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }

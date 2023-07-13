1621dd42b3778867ef7941749df0ee835e92bf42
KyakuwaPeninnah <kyakuwapeninnah@gmail.com>
2023-07-13 11:28:17 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsined long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsined long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsined long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", (unsined long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsined long)sizeof(f));
+printf("Size of a long int: %lu bytes(s)\n", (unsined long)sizeof(c));
+printf("Size of a long long int: %lu bytes(s)\ni", (unsined long)sizeof(d));
+printf("Size of a float: %lu bytes(s)\n", (unsined long)sizeof(f));
 return (0);
 }

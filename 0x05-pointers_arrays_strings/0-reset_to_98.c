a6f5c5ed156e4e3a908b72f4482e0d02be27708c
KyakuwaPeninnah <kyakuwapeninnah@gmail.com>
2023-07-24 00:36:47 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+inlude "main.h"
+/**
+  * reset_to_98 - takes a pointer to an int as parameter
+  * @n: input an integer counter
+  * Return: Nothing
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

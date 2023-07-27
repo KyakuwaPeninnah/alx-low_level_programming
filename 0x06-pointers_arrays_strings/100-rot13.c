9b49a388e90989ddd2d20a6c65cb8d8e5c9764aa
KyakuwaPeninnah <kyakuwapeninnah@gmail.com>
2023-07-27 00:42:18 -0700

    . rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * rot13 - encodes a string
+  * @str: string to be encoded
+  * Return: str
+  */
+char *rot13(char *str)
+{
+	int i, j;
+	char c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char d[] = "NOPQRSTUVWZYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (str[i] == c[j])
+			{
+				str[i] = d[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}

ff5cee774e88d1f364fa2d01086472ea696df113
KyakuwaPeninnah <kyakuwapeninnah@gmail.com>
2023-09-04 11:37:59 -0700

    Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: file to append content to
+ * @text_content: content to append to a file
+ * Return: 1 on success and -1 if it is not
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nchars;
+	int rwr;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+		if (fd == -1)
+			return (-1);
+
+	if (text_content)
+	{
+		for (nchars = 0; text_content[nchars]; nchars++);
+		rwr = write(fd, text_content, nchars);
+
+		if (rwr == -1)
+			return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
